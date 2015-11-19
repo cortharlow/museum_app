f492de1082cef768c596b397042019621a07ac74
Cort Harlow <cort.harlow@gmail.com>
2015-11-19 13:47:42 -0500

    Added Painting model

--- models/painting.js
+++ models/painting.js
@@ -1,1 +1,12 @@
 'use strict';
+let mongoose = require('mongoose');
+
+let paintingSchema = new mongoose.Schema({
+  title: String,
+  img_url: String,
+  year_made: Number
+});
+
+let Painting = mongoose.model('Painting', paintingSchema);
+
+module.exports = Painting;
