525201becd1b68d418ec2731616a2b8ffb1b2bfb
Maggie Love <maggie.love1991@gmail.com>
2015-11-19 13:47:58 -0500

    added Artist model

--- models/artist.js
+++ models/artist.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let mongoose = require('mongoose');
+
+let artistSchema = new mongoose.Schema({
+  name: String,
+  img_url: String,
+  nationality: String,
+  birthYear: Number,
+  description: String
+})
+
+let Artist = mongoose.model('Artist', artistSchema);
+
+module.exports = Artist;
